5723cfda5c8a530c239492ffcab38054c6d61ff5
azziimalek <malek.azzi@transformatek.dz>
2024-12-05 15:25:10 +0000

    Add starter app2 server and update readme

--- app2-server/src/app2.py
+++ app2-server/src/app2.py
@@ -4,7 +4,7 @@ import os
 
 # Initialisation de l'application Flask
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key')  # Clé secrète pour Flask
+app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key') 
 
 # Initialisation de l'extension OAuth
 oauth = OAuth(app)
@@ -12,9 +12,9 @@ oauth = OAuth(app)
 # Enregistrement du client Keycloak
 keycloak = oauth.register(
     name='keycloak',
-    client_id='your-client-id',  # Remplacez par votre client_id Keycloak
-    client_secret='your-client-secret',  # Remplacez par votre client_secret Keycloak
-    server_metadata_url='http://localhost:8080/realms/your-realm/.well-known/openid-configuration',  # URL de configuration de Keycloak
+    client_id='app2-client',  
+    client_secret='RucCUHrjR9zutmQD04p6dWqDH4QcA18R', 
+    server_metadata_url='http://localhost:8080/realms/Authentification/.well-known/openid-configuration', 
     client_kwargs={'scope': 'openid profile email'}
 )
 
@@ -47,8 +47,12 @@ def profile():
 
 @app.route('/logout')
 def logout():
-    session.clear()  # Effacer la session de l'utilisateur
+    session.clear() 
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=7000)  
+
+
+
+
--- app2-server/src/config.py
+++ app2-server/src/config.py
@@ -82,25 +82,114 @@ LANGUAGES = {
     "fr": {"flag": "fr", "name": "French"},
 }
 
+from datetime import timedelta
+import os
+from flask_appbuilder.security.manager import (
+    AUTH_OID,
+    AUTH_REMOTE_USER,
+    AUTH_DB,
+    AUTH_LDAP,
+    AUTH_OAUTH,
+)
+# from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Your App secret key
+SECRET_KEY = "abcdefghijklmnopqrtu"
+
+# The SQLAlchemy connection string.
+SQLLITE_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+SQLALCHEMY_DATABASE_URI = os.getenv(
+    "SQLALCHEMY_DATABASE_URI", SQLLITE_DATABASE_URI
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Flask-WTF flag for CSRF
+CSRF_ENABLED = False
+FAB_API_SWAGGER_UI = True
+# FAB_OPENAPI_SERVERS = [
+#     {"url": "http://localhost:5000/"},
+# ]
+
+# ------------------------------
+# GLOBALS FOR APP Builder
+# ------------------------------
+# Uncomment to setup Your App name
+# APP_NAME = "My App Name"
+
+# Uncomment to setup Setup an App icon
+# APP_ICON = "static/img/logo.jpg"
+
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
+# ----------------------------------------------------
+# AUTHENTICATION CONFIG
+# ----------------------------------------------------
+# The authentication type
+# AUTH_OID : Is for OpenID
+# AUTH_DB : Is for database (username/password()
+# AUTH_LDAP : Is for LDAP
+# AUTH_REMOTE_USER : Is for using REMOTE_USER from web server
+AUTH_TYPE = AUTH_OAUTH
+
+# Uncomment to setup Full admin role name
+# AUTH_ROLE_ADMIN = 'Admin'
+
+# Uncomment to setup Public role name, no authentication needed
+AUTH_ROLE_PUBLIC = "Public"
+
+# Will allow user self registration
+# AUTH_USER_REGISTRATION = True
+
+# The default user self registration role
+# AUTH_USER_REGISTRATION_ROLE = "Public"
+
+# When using LDAP Auth, setup the ldap server
+# AUTH_LDAP_SERVER = "ldap://ldapserver.new"
+
+# Uncomment to setup OpenID providers example for OpenID authentication
+# OPENID_PROVIDERS = [
+#    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+#    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+#    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+#    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+# ---------------------------------------------------
+# Babel config for translations
+# ---------------------------------------------------
+# Setup default language
+BABEL_DEFAULT_LOCALE = "en"
+# Your application default translation path
+BABEL_DEFAULT_FOLDER = "translations"
+# The allowed translation for you app
+LANGUAGES = {
+    "en": {"flag": "gb", "name": "English"},
+    "fr": {"flag": "fr", "name": "French"},
+}
+
 OAUTH_PROVIDERS = [
     {
         'name': 'keycloak',
-        'token_key': 'access_token',  # Clé pour extraire le token
-        'icon': 'fa-key',  # Icône pour l'interface utilisateur
+        'token_key': 'access_token',
+        'icon': 'fa-key',
         'remote_app': {
-            'client_id': 'app1-client',  # Client ID de votre client Keycloak
-            'client_secret': None,  # Si le client est "Public", laissez vide
+            'client_id': 'app2-client',
+            'client_secret': 'RucCUHrjR9zutmQD04p6dWqDH4QcA18R',
             'api_base_url': 'http://localhost:8080/realms/Authentification/protocol/openid-connect/',
             'client_kwargs': {'scope': 'openid email profile'},
             'access_token_url': 'http://localhost:8080/realms/Authentification/protocol/openid-connect/token',
             'authorize_url': 'http://localhost:8080/realms/Authentification/protocol/openid-connect/auth',
             'userinfo_endpoint': 'http://localhost:8080/realms/Authentification/protocol/openid-connect/userinfo',
+            'redirect_uri': [
+            "http://localhost:7000/*"
+        ],
         },
     }
 ]
+
 
 # Rôle administrateur
 AUTH_ROLE_ADMIN = "Admin"
 
 # Rôle par défaut pour les utilisateurs non connectés
 AUTH_ROLE_PUBLIC = "Public"
+
