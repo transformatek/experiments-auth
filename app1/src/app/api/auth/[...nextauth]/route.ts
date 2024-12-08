19a00b79973c41b67c4a907d0cb0b01632416724
Sedjai Bouchra <bouchra.sedjai@transformatek.dz>
2024-12-08 16:02:24 +0000

    Update  api route & update package.

--- app1/src/app/api/auth/[...nextauth]/route.ts
+++ app1/src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,31 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import KeycloakProvider from 'next-auth/providers/keycloak';
 
-// Define the `authOptions`
+console.log(process.env.KEYCLOAK_CLIENT_ID,
+    process.env.KEYCLOAK_CLIENT_SECRET,
+    process.env.KEYCLOAK_ISSUER);
+
 export const authOptions: NextAuthOptions = {
     providers: [
         KeycloakProvider({
             clientId: process.env.KEYCLOAK_CLIENT_ID!,
             clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!,
-            issuer: 'http://localhost:8080/auth/realms/my-app-realm',
+            issuer: process.env.KEYCLOAK_ISSUER!,
         }),
     ],
+    // callbacks: {
+    //     async jwt({ token, account }) {
+    //         if (account) {
+    //             token.accessToken = account.access_token;
+    //             token.refreshToken = account.refresh_token;
+    //             token.accessTokenExpires = account.expires_at ? account.expires_at * 1000 : undefined;
+    //         }
+    //         return token;
+    //     },
+    // },
 };
 
-// Export the default NextAuth handler
-export default NextAuth(authOptions);
+
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST }
