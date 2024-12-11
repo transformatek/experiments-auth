66def5dee5b7e6877a4c02fb67a3d495ef3d0da9
Sedjai Bouchra <bouchra.sedjai@transformatek.dz>
2024-12-11 15:19:35 +0000

    Add provider& sessionGuard.

---
+++ app1-web/src/app/providers.tsx
@@ -0,0 +1,13 @@
+// src/app/providers.tsx
+'use client'
+
+import { SessionProvider } from "next-auth/react"
+import { ReactNode } from "react"
+
+export function Providers({ children }: { children: ReactNode }) {
+    return (
+        <SessionProvider refetchInterval={4 * 60}>
+            {children}
+        </SessionProvider>
+    )
+}
---
+++ app1-web/src/components/sessionGuard.tsx
@@ -0,0 +1,15 @@
+// src/components/SessionGuard.tsx
+"use client";
+import { signIn, useSession } from "next-auth/react";
+import { ReactNode, useEffect } from "react";
+
+export default function SessionGuard({ children }: { children: ReactNode }) {
+    const { data } = useSession();
+    useEffect(() => {
+        if (data?.expires === "RefreshAccessTokenError") {
+            signIn("keycloak");
+        }
+    }, [data]);
+
+    return <>{children}</>;
+}
